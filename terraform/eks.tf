b756a21de881766eeef6c80da07e12b985b28350
Abhishek <stoicorion22@gmail.com>
2026-05-31 21:20:14 +0530

    clean

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -5,10 +5,10 @@ module "eks" {
   cluster_name    = "phoenix-cluster"
   cluster_version = "1.30"
 
-  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = true
 
-  vpc_id = module.vpc.vpc_id
-
+  vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
